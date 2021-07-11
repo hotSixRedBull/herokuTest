b580499f3dbeaf0f458ebd94d60c297d81128c63
Alan Seunggi Lee <software.lee@samsung.com>
2021-07-11 19:56:46 +0900

    prototype

---
+++ react-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src= "/p.png" className="App-logo" alt="logo" />
+        <p>
+          째누바보~~~
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
